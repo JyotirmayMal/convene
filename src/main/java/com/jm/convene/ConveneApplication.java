465a099308969b4df83de398b73d18595c34a1c9
JyotirmayMal <jyotirmaymal02@gmail.com>
2024-06-16 16:03:19 +0530

    first commit

---
+++ src/main/java/com/jm/convene/ConveneApplication.java
@@ -0,0 +1,13 @@
+package com.jm.convene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConveneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jm/convene/ConveneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.convene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
